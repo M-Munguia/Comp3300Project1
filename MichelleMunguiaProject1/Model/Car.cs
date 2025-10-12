261898a3cb3739f741a91d36ec6694c8cde678ee
M <munguiamichelle03@gmail.com>
2025-10-12 14:26:10 -0400

    Defined methods in Car Lot class and debugging

--- MichelleMunguiaProject1/Model/Car.cs
+++ MichelleMunguiaProject1/Model/Car.cs
@@ -4,10 +4,10 @@
     {
         private string _model = string.Empty;
         private string _make = string.Empty;
-        private double _mpg; 
-        private double _price;
+        private decimal _mpg; 
+        private decimal _price;
 
-        public Car(string make, string model, double mpg, double price)
+        public Car(string make, string model, decimal mpg, decimal price)
         {
             Make = make;
             Model = model;
@@ -37,25 +37,25 @@
             }
         }
 
-        public double Mpg
+        public decimal Mpg
         {
             get => _mpg;
             set
             {
                 const string msg = "MPG cannot be less than 0";
-                if (value is < 0)
+                if (value < 0)
                     throw new ArgumentOutOfRangeException(nameof(Mpg), msg);
                 _mpg = value;
             }
         }
 
-        public double Price
+        public decimal Price
         {
             get => _price;
             set
             {
                 const string msg = "Score must be between 0 and 100.";
-                if (value is < 0 )
+                if (value < 0 )
                     throw new ArgumentOutOfRangeException(nameof(Price), msg);
                 _price = value;
             }
--- MichelleMunguiaProject1/Model/CarLot.cs
+++ MichelleMunguiaProject1/Model/CarLot.cs
@@ -1,49 +1,101 @@
-﻿namespace MichelleMunguiaProject1.Model
+﻿namespace MichelleMunguiaProject1.Model;
+
+public class CarLot
 {
-    public class CarLot
+    public const decimal TaxRate = 0.078m;
+    public List<Car> Inventory;
+
+    public CarLot()
     {
-        public List<Car> Inventory;
+        Inventory = new List<Car>();
+        StockLotWithDefaultInventory();
+    }
 
-        public const double TaxRate = 0.078;
+    public void StockLotWithDefaultInventory()
+    {
+        Inventory.Add(new Car("Ford", "Focus ST", 28.3m, 26298.98m));
+        Inventory.Add(new Car("Chevrolet", "Camaro ZL1", 19m, 65401.23m));
+        Inventory.Add(new Car("Honda", "Accord Sedan EX", 30.2m, 26780m));
+        Inventory.Add(new Car("Lexus", "ES 350", 24.1m, 42101.10m));
+    }
 
-        public CarLot()
-        {
-            Inventory = new List<Car>();
-            StockLotWithDefaultInventory();
-        }
+    public List<Car> FindCarsByMake(string make)
+    {
+        var matchingMakeCars =
+            Inventory.Where(car => car.Make.Equals(make, StringComparison.OrdinalIgnoreCase)).ToList();
+        return (matchingMakeCars.Count > 0 ? matchingMakeCars : null) ?? throw new InvalidOperationException();
+    }
 
-        public void StockLotWithDefaultInventory()
-        {
-            Inventory.Add(new Car("Ford", "Focus ST", 28.3, 26298.98));
-            Inventory.Add(new Car("Chevrolet", "Camaro ZL1", 19, 65401.23));
-            Inventory.Add(new Car("Honda", "Accord Sedan EX", 30.2, 26780));
-            Inventory.Add(new Car("Lexus", "ES 350", 24.1, 42101.10));
-        }
+    public Car? FindCarByMakeModel(string make, string model)
+    {
+        const string msg = "make and Model cannot be null";
 
-        public List<Car> FindCarsByMake(string make)
-        {
+        if (string.IsNullOrEmpty(make) || string.IsNullOrEmpty(model))
+            throw new ArgumentNullException(msg);
+
+        var matchedCar = Inventory.FirstOrDefault(car =>
+            car.Make.Equals(make, StringComparison.OrdinalIgnoreCase) &&
+            car.Model.Equals(model, StringComparison.OrdinalIgnoreCase));
+
+        return matchedCar;
+    }
+
+    public Car? PurchaseCar(string make, string model)
+    {
+        var purchasedCar = Inventory.FirstOrDefault(c =>
+            c.Make.Equals(make, StringComparison.OrdinalIgnoreCase) &&
+            c.Model.Equals(model, StringComparison.OrdinalIgnoreCase));
+
+        if (purchasedCar == null) return null;
+        Inventory.Remove(purchasedCar);
+        return purchasedCar;
+    }
+
+    public void AddCar(string make, string model, decimal mpg, decimal price)
+    {
+        var car = new Car(make, model, mpg, price);
+        Inventory.Add(car);
+    }
+
+    public decimal GetTotalCostOfPurchase(Car car)
+    {
+        var totalTax = car.Price * TaxRate;
+        var totalCost = car.Price + totalTax;
+
+        return totalCost;
+    }
+
+    public Car? FindLeastExpensiveCar()
+    {
+        if (Inventory.Count == 0)
             return null;
-        }
 
-        public List<Car> FindCarByMakeModel(string make, string model)
-        {
+        var cheapestCar = Inventory[0];
+
+        foreach (var car in Inventory.Where(car => car.Price < cheapestCar.Price)) cheapestCar = car;
+
+        return cheapestCar;
+    }
+
+    public Car? FindMostExpensiveCar()
+    {
+        if (Inventory.Count == 0)
             return null;
-        }
 
-        public List<Car> PurchaseCar(string make, string model)
-        {
-            return null;
-        }
+        var expensiveCar = Inventory[0];
 
-        public void AddCar(Car car)
-        {
-            Inventory.Add(car);
-        }
+        foreach (var car in Inventory.Where(car => car.Price > expensiveCar.Price)) expensiveCar = car;
 
-        public void RemoveCar(Car car)
-        {
-            Inventory.Remove(car);
-        }
+        return expensiveCar;
+    }
 
+    public Car? FindBestMPGCar()
+    {
+        return null;
+    }
+
+    public Car? FindWorstMPGCar()
+    {
+        return null;
     }
 }
